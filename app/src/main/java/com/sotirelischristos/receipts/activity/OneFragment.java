8bba5fb60abb0dc74f928af544b76ad4bb9fd643
sotirelisc <gtakriz@hotmail.com>
2015-10-23 14:00:00 +0300

    Get location from devise using FusedLocationAPI.

--- app/src/main/java/com/sotirelischristos/receipts/activity/OneFragment.java
+++ app/src/main/java/com/sotirelischristos/receipts/activity/OneFragment.java
@@ -1,7 +1,8 @@
 package com.sotirelischristos.receipts.activity;
 
-import android.support.v4.app.Fragment;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,6 +15,10 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.sotirelischristos.receipts.R;
 import com.sotirelischristos.receipts.app.MyApp;
 import com.sotirelischristos.receipts.helper.Place;
@@ -25,25 +30,36 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OneFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class OneFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
 
+    // Foursquare API config
     private final String fs_client_id = "GBEDG1W53A2XFD5UN1JQSU5QDSA1WIZ0JU04KFULEKACIVBA";
     private final String fs_client_secret = "N4Z23VVUKGDLV2DAP0EGALD1ONPBIGOFN5ETSX3I1JLWMOKV";
     private String TAG = MainActivity.class.getSimpleName();
     private String fs_limit = "15";
     private String fs_range = "100";
-    private String URL = "https://api.foursquare.com/v2/venues/search?ll=37.792086,26.704900&limit=" + fs_limit + "&v=20151022&client_id=" + fs_client_id + "&client_secret=" + fs_client_secret;
-
+    private String BASE_URL = "https://api.foursquare.com/v2/venues/search?limit=" + fs_limit + "&v=20151022&client_id=" + fs_client_id + "&client_secret=" + fs_client_secret;
     private SwipeRefreshLayout swipeRefreshLayout;
     private ListView listView;
     private SwipeListAdapter adapter;
     private List<Place> placeList;
+
+    // Location services config
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
+    private Location mLastLocation;
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
 
     public OneFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Check availability of GooglePlayServices
+        if (checkPlayServices()) {
+            buildGoogleAPIClient();
+        }
     }
 
     @Override
@@ -52,13 +68,18 @@ public class OneFragment extends Fragment implements SwipeRefreshLayout.OnRefres
         if (!placeList.isEmpty()) {
             placeList.clear();
         }
-        fetchPlaces();
+        fetchPlaces(getLocation());
     }
 
     /**
      * Fetch places from Foursquare
      */
-    private void fetchPlaces() {
+    private void fetchPlaces(Location loc) {
+        double lat = loc.getLatitude();
+        double lon = loc.getLongitude();
+        Log.e(TAG, "Latitude: " + lat + " - Longitude: " + lon);
+        // Append location coordinates to request URL
+        String URL = BASE_URL + "&ll=" + String.valueOf(lat) + "," + String.valueOf(lon);
         // Show refresh animation
         swipeRefreshLayout.setRefreshing(true);
         // Make request to Foursquare API through Volley
@@ -119,16 +140,89 @@ public class OneFragment extends Fragment implements SwipeRefreshLayout.OnRefres
         /**
          * Showing Swipe Refresh animation on activity create
          * As animation won't start on onCreate, post runnable is used
-         */
         swipeRefreshLayout.post(new Runnable() {
 
                                     @Override
                                     public void run() {
                                         swipeRefreshLayout.setRefreshing(true);
-                                        fetchPlaces();
+                                        fetchPlaces(getLocation());
                                     }
                                 }
         );
+        */
+    }
+
+    /**
+     * Get the location
+     * */
+    private Location getLocation() {
+        mLastLocation = LocationServices.FusedLocationApi
+                .getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            return mLastLocation;
+        }
+        return null;
+    }
+
+    /**
+     * Create Google API client
+     * */
+    protected synchronized void buildGoogleAPIClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    /**
+     * Verify GooglePlayServices on device
+     * */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil
+                .isGooglePlayServicesAvailable(getContext());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getContext(),
+                        "This device is not supported.", Toast.LENGTH_LONG)
+                        .show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
+                + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        // Get location once connected to Google API
+        fetchPlaces(getLocation());
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
     }
 
 }
